36dc9153bbd580db3a6755c95000e34e81574bbf
Jhonatan Jaimes <jejo.arch@gmail.com>
2025-06-04 19:35:07 -0500

    Primer commit python

---
+++ encriptar_desencriptar.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+biblioteca_texto = {
+    # Minúsculas (a-z) -> 10-35
+    'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15,
+    'g': 16, 'h': 17, 'i': 18, 'j': 19, 'k': 20, 'l': 21,
+    'm': 22, 'n': 23, 'o': 24, 'p': 25, 'q': 26, 'r': 27,
+    's': 28, 't': 29, 'u': 30, 'v': 31, 'w': 32, 'x': 33,
+    'y': 34, 'z': 35,
+
+    # Mayúsculas (A-Z) -> 36-61
+    'A': 36, 'B': 37, 'C': 38, 'D': 39, 'E': 40, 'F': 41,
+    'G': 42, 'H': 43, 'I': 44, 'J': 45, 'K': 46, 'L': 47,
+    'M': 48, 'N': 49, 'O': 50, 'P': 51, 'Q': 52, 'R': 53,
+    'S': 54, 'T': 55, 'U': 56, 'V': 57, 'W': 58, 'X': 59,
+    'Y': 60, 'Z': 61, ' ': 91
+}
+
+biblioteca_numeros = {
+    "10": 'a', "11": 'b', "12": 'c', "13": 'd', "14": 'e', "15": 'f',
+    "16": 'g', "17": 'h', "18": 'i', "19": 'j', "20": 'k', "21": 'l',
+    "22": 'm', "23": 'n', "24": 'o', "25": 'p', "26": 'q', "27": 'r',
+    "28": 's', "29": 't', "30": 'u', "31": 'v', "32": 'w', "33": 'x',
+    "34": 'y', "35": 'z',
+
+    "36": 'A', "37": 'B', "38": 'C', "39": 'D', "40": 'E', "41": 'F',
+    "42": 'G', "43": 'H', "44": 'I', "45": 'J', "46": 'K', "47": 'L',
+    "48": 'M', "49": 'N', "50": 'O', "51": 'P', "52": 'Q', "53": 'R',
+    "54": 'S', "55": 'T', "56": 'U', "57": 'V', "58": 'W', "59": 'X',
+    "60": 'Y', "61": 'Z', "91": ' '
+}
+
+biblioteca_no_usados = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '63', '64', '65', '66', '67', '68', '69', '70', '71',
+    '72', '73', '74', '75', '76', '77', '78', '79', '80',
+    '81', '82', '83', '84', '85', '86', '87', '88', '89',
+    '90', '62', '92', '93', '94', '95', '96', '97', '98',
+    '99'
+}
+
+
+def encriptar(text):
+    texto_encriptado = ""
+    for char in text:
+        if char in biblioteca_texto:
+            numero = biblioteca_texto[char] * 109  # Multiplicamos
+            numero_str = str(numero)  # Aseguramos al menos 4 dígitos
+            primera_mitad = numero_str[:2]
+            segunda_mitad = numero_str[-2:]
+
+            letra1 = biblioteca_numeros.get(primera_mitad, str(primera_mitad))
+            letra2 = biblioteca_numeros.get(segunda_mitad, str(segunda_mitad))
+
+            texto_encriptado += str(letra1) + str(letra2)
+        else:
+            texto_encriptado += char  # Dejamos los caracteres no encriptables
+
+    print("  |------------------Encriptado------------------")
+    print("  |", texto_encriptado)
+    print("  |", len(texto_encriptado), "letras")
+
+
+def desencriptar(texto_encriptado):
+    texto_desencriptado = ""
+    numero = ""
+    for char in texto_encriptado:
+        if char in biblioteca_texto:
+            n = biblioteca_texto[char]
+            numero += str(n)
+        elif char in biblioteca_no_usados:
+            numero += str(char)
+
+    for i in range(0, len(numero), 4):
+        par = numero[i:i + 4]
+        par = int(par) // 109
+        par = str(par)
+        if par in biblioteca_numeros:
+            par = biblioteca_numeros[par]
+
+        texto_desencriptado += par
+
+    print("  |-----------------Desncriptado-----------------")
+    print("  |", texto_desencriptado)
+
+
+def main():
+    seguir = True
+    while seguir:
+        print("")
+        print("   ----------------------------------------------")
+        opcion = input("  | Que desea realizar encriptar o desencriptar: Opciones [ e / d ] ")
+        if opcion.lower() == 'e':
+            encriptar(input("  | Ingrese palabras: "))
+            print("   ----------------------------------------------")
+        elif opcion.lower() == 'd':
+            desencriptar(input("  | Ingrese lo encriptado: \n  | "))
+            print("   ----------------------------------------------")
+        else:
+            print("   ----------------------------------------------")
+            print("")
+            seguir = False
+
+
+if __name__ == '__main__':
+    main()
---
+++ hipoteca_tiempo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def main():
+    print(" ")
+    print("    --------------------------------")
+    print("    | Calcular hipoteca fija")
+    print("    --------------------------------")
+    dinero_pagar: float = float(input("    | Monto a pagar: "))
+    cantidad_years: int = int(input("    | Cantidad de años: "))
+    tasa_interes_anual: float = float(input("    | Tasa de interes anual: "))
+
+    cantidad_meses = cantidad_years * 12
+    tasa_interes_mensual = tasa_interes_anual / 12 / 100
+
+    cuota = (dinero_pagar * tasa_interes_mensual * (1 + tasa_interes_mensual) ** cantidad_meses) / \
+            ((1 + tasa_interes_mensual) ** cantidad_meses - 1)
+    cuota = round(cuota, 2)
+    print("    | Cuota mensual a pagar:", cuota, "(moneda local de tu pais)")
+    print("    | Monto total despues de", cantidad_years, "años", "pagando es:", (cuota * cantidad_meses))
+    print("    --------------------------------")
+    print(" ")
+
+
+if __name__ == '__main__':
+    main()
